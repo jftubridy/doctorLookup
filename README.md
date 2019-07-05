5317e8d8cc8779eaa0c519c9e384a03af6fe84dd
Joseph Tubridy <jftubridy@gmail.com>
2019-07-05 14:02:34 -0700

    commit setup, I think I've gotten everything working right

---
+++ README.md
@@ -0,0 +1,31 @@
+# Doctor Lookup
+### By Joseph Tubridy
+
+---
+
+## Description
+*
+
+
+## Technologies Used
+* JavaScript
+* JQuery
+* HTML5
+* CSS
+* Bootsrap
+* Webpack
+
+## Installation
+* Clone repo, from the root directory, if using mac change "start": "npm run build && webpack-dev-server" in package.json to "npm run build; webpack-dev-server", install dependencies.
+
+```sh
+$ git clone
+$ cd doctorLookup
+$ npm install
+```
+
+## Specs
+
+| Behaviors       | Input          | Output      |
+| ---------------- |:------------:| :--------------:|
+| When click on  | click on  | does |
---
+++ src/docLookup.js
@@ -0,0 +1,30 @@
+// export let tamagotchi = {
+//   foodLevel: 10, // start higher when done wroking!
+//   setHunger: function() {
+//     this.foodLevel--;
+//     if (this.didYouGetEaten() == true) {
+//       return "You let your Tamagotchi DIE!";
+//     }
+//   },
+//   didYouGetEaten: function() {
+//     if (this.foodLevel > 0) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   },
+//   feed: function(amount) {
+//     if(this.foodLevel >= 100){
+//       return this.foodLevel = 100;
+//     }
+//     let that = this;
+//     return function(food) {
+//       that.foodLevel += amount
+//       return `Your Tamagotchi ate ${food}! Food level goes up ${amount}!`
+//     }
+//   }
+// };
+// tamagotchi.eatSmall = tamagotchi.feed(5);
+// tamagotchi.eatMedium = tamagotchi.feed(10);
+
+//http://www.rafaelservantez.com/writings_tutorials_web_js_update.html
