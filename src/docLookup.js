91e495d837ef2b0bbec1649c1558edb2e535c923
Joseph Tubridy <jftubridy@gmail.com>
2019-07-29 09:37:41 -0700

    seperate front and back a little bit better, still work in progress

--- src/docLookup.js
+++ src/docLookup.js
@@ -1,11 +1,23 @@
-export let docSearch = (docName, specialty);
-return new Promise(function (resolve, reject) {
-  let request = new XMLHttpRequest();
-  let resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-})
-
-$.get(resource_url, function (data) {
-  // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-  var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-  document.getElementById('content-placeholder').innerHTML = template(data);
-});
+//export  docSearch = (docName, specialty); {
+export class DocSearch {
+  docSearch(docName, specialty) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+// $.get(resource_url, function (data) {
+//   // data: { meta: {<metadata>}, data: {<array[Practice]>} }
+//   var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+//   document.getElementById('content-placeholder').innerHTML = template(data);
+// });
--- src/main.js
+++ src/main.js
@@ -2,43 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { docSearch } from './docLookup.js';
+import { DocSearch } from './docLookup.js';
 
 // let grylls = tamagotchi;
 
 $(document).ready(function () {
   $('#docName').click(function () {
-    let name = $('#docName').val();
+    let docName = $('#docName').val();
     $('#docName').val("");
 
-    let request = new XMLHttpRequest();
-    const url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,;
-
-    request.onreadystechange = function () {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
+    // letpromise = new Promise(function (resolve, reject) {
+    //   let request = new XMLHttpRequest();
+    //   const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,
 
     const getElements = function (response) {
       $('#doc-template').text(`place holder info for ${docName} and ${specialty}`);
     }
 
-    type: 'GET',
-      data: {
-      format: 'json'
-    },
-    success: function (response) {
-      $('.doc-template').text(`Information for ${name} is ${response.data}%`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
-    },
-    error: function () {
-      $('#errors').text("There was an error processing your request. Please try again.");
-    }
+    // type: 'GET',
+    //   data: {
+    //   format: 'json'
+    // },
+    // success: function (response) {
+    //   $('.doc-template').text(`Information for ${name} is ${response.data}%`);
+    //   // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+    // },
+    // error: function () {
+    //   $('#errors').text("There was an error processing your request. Please try again.");
+    // }
   });
 });
 });
