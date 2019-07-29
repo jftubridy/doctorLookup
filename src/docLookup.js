d45fc68b667baa909aa890b7d381dbb2c1a510d9
Joseph Tubridy <jftubridy@gmail.com>
2019-07-29 08:57:22 -0700

    using the weather lesson to fix doc api stuff

--- src/docLookup.js
+++ src/docLookup.js
@@ -1,43 +1,11 @@
-// export let tamagotchi = {
-//   foodLevel: 10, // start higher when done wroking!
-//   setHunger: function() {
-//     this.foodLevel--;
-//     if (this.didYouGetEaten() == true) {
-//       return "You let your Tamagotchi DIE!";
-//     }
-//   },
-//   didYouGetEaten: function() {
-//     if (this.foodLevel > 0) {
-//       return false;
-//     } else {
-//       return true;
-//     }
-//   },
-//   feed: function(amount) {
-//     if(this.foodLevel >= 100){
-//       return this.foodLevel = 100;
-//     }
-//     let that = this;
-//     return function(food) {
-//       that.foodLevel += amount
-//       return `Your Tamagotchi ate ${food}! Food level goes up ${amount}!`
-//     }
-//   }
-// };
-// tamagotchi.eatSmall = tamagotchi.feed(5);
-// tamagotchi.eatMedium = tamagotchi.feed(10);
-
-//http://www.rafaelservantez.com/writings_tutorials_web_js_update.html
-
-
-var api_key = process.env.apiKey; // Get your API key at developer.betterdoctor.com
-
-var resource_url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + api_key;
-
-
+export let docSearch = (docName, specialty);
+return new Promise(function (resolve, reject) {
+  let request = new XMLHttpRequest();
+  let resource_url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+})
 
 $.get(resource_url, function (data) {
-    // data: { meta: {<metadata>}, data: {<array[Practice]>} }
-    var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
-    document.getElementById('content-placeholder').innerHTML = template(data);
+  // data: { meta: {<metadata>}, data: {<array[Practice]>} }
+  var template = Handlebars.compile(document.getElementById('docs-template').innerHTML);
+  document.getElementById('content-placeholder').innerHTML = template(data);
 });
--- src/main.js
+++ src/main.js
@@ -2,28 +2,41 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { tamagotchi } from './docLookup.js';
+import { docSearch } from './docLookup.js';
 
 // let grylls = tamagotchi;
 
-$(document).ready(function() {
-$('#docName').click(function() {
-  let name = $('#docName').val();
-  $('#docName').val("");
-  $.ajax({
-    url: `https://api.betterdoctor.com/2016-03-01/doctors?name=doom&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=rating-desc&skip=0&limit=10&user_key=${exports.apiKey}`,
+$(document).ready(function () {
+  $('#docName').click(function () {
+    let name = $('#docName').val();
+    $('#docName').val("");
 
-//     http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=462946d0a52b8ba2f2ec97b1a4ca72e7
-// ,
+    let request = new XMLHttpRequest();
+    const url: `https://api.betterdoctor.com/2016-03-01/doctors?name=${docName}&specialty_uid=${specialty}&skip=0&limit=10&user_key=${process.env.exports.apiKey}`,;
+
+    request.onreadystechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function (response) {
+      $('#doc-template').text(`place holder info for ${docName} and ${specialty}`);
+    }
+
     type: 'GET',
-    data: {
+      data: {
       format: 'json'
     },
-    success: function(response) {
+    success: function (response) {
       $('.doc-template').text(`Information for ${name} is ${response.data}%`);
-     // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
+      // $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp}.`);
     },
-    error: function() {
+    error: function () {
       $('#errors').text("There was an error processing your request. Please try again.");
     }
   });
